0e067c075bddd536db568e23e750cf4ae9c2e982
Daniel Cabrera <daniel.cch94@hotmail.com>
2023-07-18 14:05:09 -0500

    iOS implementation added

---
+++ ios/Classes/SensorHandler.swift
@@ -0,0 +1,44 @@
+import Flutter
+import Foundation
+import CoreLocation
+
+/// Handles sensor data and dispatches data to flutter event
+class SensorHandler: NSObject, FlutterStreamHandler, CLLocationManagerDelegate {
+    private var eventSink: FlutterEventSink?;
+    private let locationManager: CLLocationManager
+
+    /// Initializes [CLLocationManager] and request location authorization
+    override init() {
+        locationManager = CLLocationManager()
+        super.init()
+
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+    /// Start Heading update if heading available or sends error if heading not available
+    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        eventSink = events;
+
+        if(CLLocationManager.headingAvailable()) {
+            locationManager.startUpdatingHeading()
+        }
+
+        return FlutterError(code: "NO_SENSOR", message: "Sensor not found", details: "Device don't have required sensor")
+    }
+
+    /// Disposes of [CLLocationManager] asn [FlutterEventSink] when connection is over
+    func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        locationManager.stopUpdatingHeading()
+        locationManager.delegate = nil
+        eventSink = nil;
+
+        return nil
+    }
+
+    /// Updates location and sends it to FlutterEventSink
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        let orientation = round(newHeading.magneticHeading * 100) / 100
+        eventSink?(orientation)
+    }
+}
--- ios/Classes/SwiftGalacticCompassPlugin.swift
+++ ios/Classes/SwiftGalacticCompassPlugin.swift
@@ -1,14 +1,21 @@
 import Flutter
 import UIKit
 
+/// Registers MethodChannel and EventChannel
 public class SwiftGalacticCompassPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "galactic_compass", binaryMessenger: registrar.messenger())
-    let instance = SwiftGalacticCompassPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let sensorHandler = SensorHandler()
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+        let channel = FlutterMethodChannel(name: "com.danielcch.galactic_compass.channel", binaryMessenger: registrar.messenger())
+
+        let orientationChannel = FlutterEventChannel(name: "com.danielcch.galactic_compass.orientation", binaryMessenger: registrar.messenger())
+        orientationChannel.setStreamHandler(sensorHandler)
+
+        let instance = FlCompassPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+
+    }
 }
